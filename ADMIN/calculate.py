4bbe6eb573834d222482fd181d6b1dcba98258c3
1DS22CS223-SUPRIYAR <137993833+1DS22CS223-SUPRIYAR@users.noreply.github.com>
2024-02-14 18:06:21 +0530

    Delete ADMIN directory

--- ADMIN/calculate.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime, timedelta
-
-def get_consecutive_dates():
-    today = datetime.now().date()
-    return [(today + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(3)]
